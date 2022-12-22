f84ae3e65c1049b703f7d69bd82db1048ac04276
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2022-12-22 18:38:51 +0100

    ciao

---
+++ Assets/Strada.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d36746e9d212954401cf0e883528bb600c799945d38b8d4ed4862099cca42ad9
+size 607
---
+++ Assets/acatBus.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba7702a20ed3697dba4b27b0e4574611f38d3455676d2717169f29e9e9ddc61c
+size 1181
